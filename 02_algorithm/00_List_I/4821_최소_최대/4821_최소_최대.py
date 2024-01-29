3bf67557f3a9928915875bc7b0d7efcb44f70655
구현우 <kjkkovekhw@naver.com>
2024-01-29 22:10:09 +0900

    알고리즘 과제

--- 02_algorithm/00_List_I/4821_최소_최대/4821_최소_최대.py
+++ 02_algorithm/00_List_I/4821_최소_최대/4821_최소_최대.py
@@ -1,11 +1,45 @@
 import sys
 sys.stdin = open('sample_input.txt')
 
-T = int(input())
-for i in range(1,T+1):
-     N = int(input())
-     a = list(map(int, input().split()))
-     result = max(a) - min(a)
-     print(f'#{i} {result}')
+t = int(input())
+
+for i in range(1,t+1):  # 1부터 t까지 반복해주겠다.
+    N = int(input())  # 
+    a = list(map(int, input().split()))
+    result = max(a) - min(a)
+    print(f'#{i} {result}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# T = int(input())
+# for i in range(1,T+1):
+#      N = int(input())
+#      a = list(map(int, input().split()))
+#      result = max(a) - min(a)
+#      print(f'#{i} {result}')
 
 
--- 02_algorithm/00_List_I/4835_구간합/4835_구간합.py
+++ 02_algorithm/00_List_I/4835_구간합/4835_구간합.py
@@ -1,7 +1,37 @@
 import sys
 sys.stdin = open('sample_input.txt')
 
-T = int(input())
-N , M = int(input().split())
-for i in range(1, T+1):
-    a = int(input())
+t = int(input())
+
+for i in range(1, t+1):
+    N , M = map(int, input().split())
+    numbers = list(map(int, input().split()))
+    # numbers 해준이유 
+    # 배열 된 정수를 list에 담아줌.
+    result = []
+    # 빈 리스트를 하나 만들어줌.
+    for j in range(N - M + 1):
+        # j가 전체 한번씩 돌수 있음.
+        total = sum(numbers[j:j+M])
+        result.append(total)
+
+    max_sum = max(result)
+    min_sum = min(result)
+
+    print(f'#{i} {max_sum-min_sum}')
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
