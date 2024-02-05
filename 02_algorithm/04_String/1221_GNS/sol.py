fa81ac565676e00e14a79389325b745bccde41b5
구현우 <kjklovekhw@gmail.com>
2024-02-05 17:49:15 +0900

    20240205

--- 02_algorithm/04_String/1221_GNS/sol.py
+++ 02_algorithm/04_String/1221_GNS/sol.py
@@ -3,3 +3,10 @@ sys.stdin = open('input.txt')
 
 T = int(input())
 
+string = ['ZRO','ONE','TWO','THR','FOR','FIV','SIX','SVN','EGT','NIN']
+
+for _ in range(1, T+1):
+    n, num = map(int, input().split())
+    
+
+
--- python/1차원 배열/2562/2562.py
+++ python/1차원 배열/2562/2562.py
@@ -1,3 +1,10 @@
 import sys
 sys.stdin = open('input.txt')
 
+numbers = []
+for _ in range(9):
+    i = int(input())
+    numbers.append(i)
+
+print(max(numbers))
+print(numbers.index(max(numbers)) + 1)
