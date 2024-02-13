e587038931ac9810f251391a7e13b143f3d92fc1
구현우 <kjklovekhw@gmail.com>
2024-02-13 17:54:14 +0900

    20240213

--- 02_algorithm/08_Stack_II/1222_계산기1/sol.py
+++ 02_algorithm/08_Stack_II/1222_계산기1/sol.py
@@ -1,3 +1,10 @@
 import sys
 sys.stdin = open('input.txt')
 
+for test_case in range(1, 11):
+    num_len = int(input())
+    num = list(map(int, input().split('+')))
+
+    print(f'#{test_case} {sum(num)}')
+
+    
--- 02_algorithm/08_Stack_II/4875_미로/sol.py
+++ 02_algorithm/08_Stack_II/4875_미로/sol.py
@@ -1,9 +1,54 @@
 import sys
 sys.stdin = open('input.txt')
 
+'''
+N X N 크기의 미로에서 출발지에서 목적지에 도착하는 경로가
+존재하는지 확인하는 프로그램을 작성하시오.
+도착할 수 있으면 1, 아니면 0을 출력한다
+2는 출발지이며 3은 도착지이며 0은 통로 1은 벽이다.
+'''
+
 T = int(input())
 
 for tc in range(1, T+1):
     N = int(input())
-    arr = [list(map(int, input().split())) for _ in range(N)]
+    arr = [list(map(int, input())) for _ in range(N)]
+    stack = []
+    visited = [[False ] * N for _ in range(N)]
 
+    dx = [1,0,-1,0]
+    dy = [0,1,0,-1]
+
+    for i in range(N):
+        for j in range(N):
+            if arr[i][j] == 2:
+                start_i = i
+                start_j = j
+
+            if arr[i][j] == 3:
+                goal_i = i
+                goal_j = j
+    
+    stack.append([start_i, start_j])
+
+    while stack:
+        i,j = stack.pop()
+        for k in range(4):
+            nx = dx[k] + i
+            ny = dy[k] + j
+
+            if nx == goal_i and ny == goal_j:
+                visited[nx][ny] = True
+                break
+
+            if 0 <= nx < N and 0 <= ny < N:
+                if arr[nx][ny] == 0 and visited[nx][ny] == False:
+                    stack.append([nx, ny])
+                    visited[nx][ny] = True
+    
+    if visited[goal_i][goal_j] == True:
+        print(f'#{tc} 1')
+    else:
+        print(f'#{tc} 0')
+                    
+            
