63a5cb3cf57c7638a753c83add0dd5a6a8042ded
구현우 <kjklovekhw@gmail.com>
2024-02-21 23:48:49 +0900

    20240221

--- BOJ/집합과 맵/10815/sol.py
+++ BOJ/집합과 맵/10815/sol.py
@@ -1,3 +1,26 @@
 import sys
 sys.stdin = open('input.txt')
 
+'''
+상근이가 가지고 있으면 1을 아니면 0을 출력해라
+시간 초과 뜸......
+'''
+
+N = int(input())
+N_number = list(map(int, input().split()))
+M = int(input())
+M_number = list(map(int, input().split()))
+
+a = []
+b = []
+result = []
+for i in range(N):
+    a.append(N_number[i])
+for j in range(M):
+    b.append(M_number[j])
+    if b[j] in a:
+        result.append(1)
+    else:
+        result.append(0)
+
+print(*result)
---
+++ BOJ/집합과 맵/7785/sol.py
@@ -0,0 +1,7 @@
+import sys
+sys.stdin = open('input.txt')
+
+n = int(input())
+for tc in range(n):
+    name, go = map(str, input().split())
+    pass
