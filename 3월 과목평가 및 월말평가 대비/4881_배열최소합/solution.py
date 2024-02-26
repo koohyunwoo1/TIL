7311aa7aa33df5942a3870393f9dec583f4ada8b
구현우 <kjklovekhw@gmail.com>
2024-02-26 16:13:59 +0900

    20240226

--- 3월 과목평가 및 월말평가 대비/4881_배열최소합/solution.py
+++ 3월 과목평가 및 월말평가 대비/4881_배열최소합/solution.py
@@ -1,32 +1,11 @@
 import sys
 sys.stdin = open('input.txt')
-'''
-세로로 같은 줄에서 두 개 이상의 숫자를 고를수 없음.
-'''
-
-# T = int(input())
-
-# for tc in range(1, T+1):
-#     N = int(input())
-#     lst = [list(map(int, input().split())) for _ in range(N)]
-
-#     min_sum = 0
-
-#     for i in range(N):
-#         min_sum += min(lst[i])
-#         min_index = lst[i].index(min(lst[i]))
-#         # print(min_index)
-#         for j in range(N):
-#             for k in range(N):
-#                 if j == min_index and k != i:
-#                     lst[k][j] = 9999
-
-#     print(lst)
-
-#     print(f'#{tc} {min_sum}')
-
 
 def backtrack(row, n, now_sum, visited):
+    # row : 현재 탐색 중인 행의 인덱스
+    # n : 행렬의 크기
+    # now_sum : 현재까지 선택한 요소들의 합
+    # visited : 각 열을 방문했는지 여부를 나타내는 리스트
     global min_sum  # 최소합을 전역변수로 선언
     if row == n:  # i가 배열의 수와 일치하면
         # 현재 합과 (지금까지)최소합 값을 비교
@@ -53,3 +32,24 @@ for tc in range(T):
     backtrack(0, n, 0, visited)  # 함수시작
     print(f'#{tc + 1} {min_sum}')
 
+
+# T = int(input())
+#
+# for tc in range(1, T+1):
+#     N = int(input())
+#     lst = [list(map(int, input().split())) for _ in range(N)]
+#
+#     min_sum = 0
+#
+#     for i in range(N):
+#         min_sum += min(lst[i])
+#         min_index = lst[i].index(min(lst[i]))
+#         # print(min_index)
+#         for j in range(N):
+#             for k in range(N):
+#                 if j == min_index and k != i:
+#                     lst[k][j] = 9999
+#
+#     print(lst)
+#
+#     print(f'#{tc} {min_sum}')
