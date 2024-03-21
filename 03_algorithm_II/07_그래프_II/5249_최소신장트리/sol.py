4ff77c2ff53b2e767203920318953a869b3e016b
구현우 <kjklovekhw@gmail.com>
2024-03-21 12:26:27 +0900

    20240321

--- 03_algorithm_II/07_그래프_II/5249_최소신장트리/sol.py
+++ 03_algorithm_II/07_그래프_II/5249_최소신장트리/sol.py
@@ -1,2 +1,52 @@
 import sys
+from heapq import heappush, heappop
 sys.stdin = open('input.txt')
+
+
+def prim(start):
+    heap = list()
+    MST = [0] * V
+
+    sum_weight = 0
+
+    heappush(heap, (0, start))
+
+    while heap:
+        weight, v = heappop(heap)
+
+        if MST[v]:
+            continue
+
+        # 방문 처리
+        MST[v] = 1
+        # 누적합 추가
+        sum_weight += weight
+
+        # 갈수 있는 노드를 보면서
+        for next_v in range(V):
+            # 갈 수 없는 지점이면 continue
+            if graph[v][next_v] == 0:
+                continue
+
+            # 이미 방문한 지점이면 continue
+            if MST[next_v]:
+                continue
+
+            heappush(heap, (graph[v][next_v], next_v))
+
+    return sum_weight
+
+T = int(input())
+
+for tc in range(1, T+1):
+    V, E = map(int, input().split())
+    graph = [[0] * V for _ in range(V)]
+    for _ in range(E):
+        s, e, w = map(int, input().split())
+        # 정점 번호를 0부터 시작하도록 조정
+        graph[s-1][e-1] = w
+        # graph[e-1][s-1] = w
+
+    result = prim(1)
+    print(f'#{tc} {result}')
+
--- 03_algorithm_II/07_그래프_II/5250_최소비용/sol.py
+++ 03_algorithm_II/07_그래프_II/5250_최소비용/sol.py
@@ -1,2 +1,52 @@
 import sys
+from collections import deque
 sys.stdin = open('input.txt')
+
+# 테스트 케이스의 개수를 입력받음
+T = int(input())
+
+# 각 테스트 케이스에 대해 반복
+for tc in range(1, T + 1):
+    # 격자의 크기를 입력받음
+    N = int(input())
+
+    # 격자 내 각 셀의 높이를 입력받음
+    height = [list(map(int, input().split())) for _ in range(N)]
+
+    # 가능한 이동 방향: 위, 아래, 오른쪽, 왼쪽
+    di = [1, -1, 0, 0]
+    dj = [0, 0, 1, -1]
+
+    # 각 셀까지의 최소 비용을 저장할 2차원 배열 초기화
+    min_cost = [[float('Inf')] * N for _ in range(N)]
+    # 시작 지점까지의 비용은 0
+    min_cost[0][0] = 0
+
+    # BFS 탐색을 위한 큐 초기화 (시작점: (0, 0))
+    queue = deque([(0, 0)])
+
+    # BFS 탐색 수행
+    while queue:
+        # 큐에서 현재 셀을 꺼냄
+        i, j = queue.popleft()
+
+        # 인접한 모든 셀을 탐색
+        for k in range(4):
+            ni = i + di[k]  # 다음 셀의 행 인덱스 계산
+            nj = j + dj[k]  # 다음 셀의 열 인덱스 계산
+
+            # 다음 셀이 격자 범위 내에 있는지 확인
+            if 0 <= ni < N and 0 <= nj < N:
+                # 다음 셀까지의 새로운 비용 계산
+                new_cost = max(0, height[ni][nj] - height[i][j]) + 1
+
+                # 다음 셀까지의 비용이 현재까지의 최소 비용보다 작으면
+                # 최소 비용을 업데이트하고 다음 셀을 큐에 추가하여 계속 탐색
+                if min_cost[i][j] + new_cost < min_cost[ni][nj]:
+                    min_cost[ni][nj] = min_cost[i][j] + new_cost
+                    queue.append((ni, nj))
+
+    # 현재 테스트 케이스에서 우하단 셀까지의 최소 비용 출력
+    print(f'#{tc} {min_cost[N - 1][N - 1]}')
+
+
