f45f3d3f6e74d806e7a5370b635d9aacf3104c58
구현우 <kjklovekhw@gmail.com>
2024-03-30 22:40:08 +0900

    20240330

--- 개인적으로 따로 공부하는_BOJ/스택, 큐, 덱/1966/sol.py
+++ 개인적으로 따로 공부하는_BOJ/스택, 큐, 덱/1966/sol.py
@@ -1,19 +1,39 @@
+# import sys
+# from collections import deque
+# read = sys.stdin.readline
+
+# T = int(read())
+#
+# for tc in range(T):
+#     N, M = map(int, read().split())
+#     important = list(map(int, read().split()))
+#
+#     queue = deque()
+#
+#     for i in range(N):
+#         if queue:
+#             queue.append(important.popleft())
+
 import sys
+sys.stdin = open('input.txt')
+
+
 from collections import deque
-read = sys.stdin.readline
 
-T = int(read())
+T = int(input())
+for _ in range(T):
+    N, M = map(int, input().split())
+    queue = deque(map(int, input().split()))
+    queue = deque([(i, idx) for idx, i in enumerate(queue)])
 
-for tc in range(T):
-    N, M = map(int, read().split())
-    important = list(map(int, read().split()))
-
-    queue = deque()
-
-    for i in range(N):
-        if queue:
-            queue.append(important.popleft())
-
-
-
-
+    count = 0
+    while True:
+        if queue[0][0] == max(queue, key=lambda x: x[0])[0]:
+            count += 1
+            if queue[0][1] == M:
+                print(count)
+                break
+            else:
+                queue.popleft()
+        else:
+            queue.append(queue.popleft())
