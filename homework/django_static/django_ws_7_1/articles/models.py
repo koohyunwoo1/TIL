ef7589b0d8cc0eb1f6f6bed4af850a74c530986d
구현우 <kjklovekhw@gmail.com>
2024-03-31 21:33:33 +0900

    20240331

---
+++ homework/django_static/django_ws_7_1/articles/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length = 100)
+    image = models.ImageField(blank=True)
+    image_description = models.TextField()
+    content = models.CharField(max_length = 250)
+    created_at = models.DateTimeField(auto_now_add = True)
+    is_public = models.BooleanField(default = True)
+    
+    
