e1da8db562ca38045a09b5a1cfe78331355499e3
구현우 <kjklovekhw@gmail.com>
2024-04-01 17:43:42 +0900

    20240401

---
+++ homework/django-authentication-system(2)/django_hw_9_4/accounts/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import get_user_model
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+User = get_user_model()
+
+# Create your views here.
+def index(request):
+    persons = User.objects.all()
+    context = {
+        'persons': persons
+    }
+    return render(request, 'accounts/index.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('accounts:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/login.html', context)
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
+
+def update(request):
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, isinstance = request)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:index')
+    else:
+        form = CustomUserChangeForm(instance = request.user)
+    context = {
+        'form' : form,
+    }
+    return render(request, 'accounts/update.html', context)
+
+
+def change_password(request, pk):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:index')
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {
+        'form' : form,
+    }
+    return render(request, 'accounts/change_password.html', context)
+
+def logout(request):
+    auth_login(request)
+    return redirect('accounts:index')
