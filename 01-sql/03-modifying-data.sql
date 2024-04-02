5c4d9092454b491b2638fadc782a194b807cde3f
구현우 <kjklovekhw@gmail.com>
2024-04-02 14:05:50 +0900

    20240402

--- 01-sql/03-modifying-data.sql
+++ 01-sql/03-modifying-data.sql
@@ -3,10 +3,36 @@ SELECT * FROM articles;
 DROP TABLE articles;
 PRAGMA table_info('articles');
 
+CREATE TABLE articles (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title VARCHAR(100) NOT NULL,
+  content VARCHAR(200) NOT NULL,
+  createdAt DATE NOT NULL
+);
+
 
 -- 1. Insert data into table
+INSERT INTO 
+  articles (title, content, createdAt)
+VALUES
+  ('hello', 'world', '2000-01-01');
 
 
+INSERT INTO 
+  articles (title, content, createdAt)
+VALUES
+  ('hello', 'DB', '2024-04-02');
+
+
+INSERT INTO 
+  articles (title, content, createdAt)
+VALUES
+  ('python', 'clear', '2024-01-20'),
+  ('algorithm', 'clear', '2024-03-16'),
+  ('django', 'in progress', '2024-04-01'),
+  ('hello', 'DB', '2024-04-02');
+
+SELECT * FROM articles;
 -- 2. Update data in table
 
 
