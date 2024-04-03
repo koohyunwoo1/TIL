87f86a0b13f3fe20455f1aa67b931ec9ba70d67f
구현우 <kjklovekhw@gmail.com>
2024-04-03 17:57:51 +0900

    20240403

---
+++ homework/01-sql/db_ws_1_b/data.sql
@@ -0,0 +1,28 @@
+SELECT
+  *
+FROM
+  songs
+
+
+SELECT
+  *
+FROM
+  songs
+ORDER BY
+  title DESC;
+
+SELECT
+  *
+FROM
+  songs
+WHERE
+  genre = 'Pop'
+
+
+SELECT
+  *
+FROM
+  songs
+WHERE
+  duration >= 180;
+
