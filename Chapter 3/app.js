9abae76742a67c0bc94fc00d7ad84b3a1ee98695
구현우 <kjklovekhw@gmail.com>
2024-04-04 16:06:27 +0900

    20240404

---
+++ Chapter 3/app.js
@@ -0,0 +1,8 @@
+const title = document.querySelector(".hello h1")
+// querySelector -> 오로지 첫번째것만 console에 띄워준다.
+// querySelectorAll -> 인자에 적합한 모든 것을 console에 출력해준다.
+
+title.innerText = "Hello";
+console.log(title);
+
+ 
--- baekjoon/app.js
+++ baekjoon/app.js
@@ -1,8 +1,8 @@
 // 1000
 
-const fs = require('fs');
-const input = fs.readFileSync('input.txt').toString().split(' ')
-console.log(typeof input)
+// const fs = require('fs');
+// const input = fs.readFileSync('input.txt').toString().split(' ')
+// console.log(typeof input)
 
 // const input = fs.readFileSync('/dev/stdin').toString().split(' ');
 // const A = parseInt(input[0])
@@ -107,3 +107,29 @@ console.log(typeof input)
 // }
 
 // const sumResult = calculator.sum(parseInt())
+
+
+
+// 9498
+
+// const fs = require('fs');
+// // const input = fs.readFileSync('input.txt').toString().split(' ');
+// const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+// const num = parseInt(input[0])
+
+// if (90<= num) {
+//   console.log('A')
+// }
+// else if (80 <= num) {
+//   console.log('B')
+// }
+// else if (70 <= num) {
+//   console.log('C')
+// }
+// else if (60 <= num) {
+//   console.log('D')
+// }
+// else {
+//   console.log('F')
+// }
+
