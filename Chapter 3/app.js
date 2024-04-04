f8cc014291e6e6c499d9fe9222ccbc86064820da
구현우 <kjklovekhw@gmail.com>
2024-04-04 16:53:00 +0900

    20240404

--- Chapter 3/app.js
+++ Chapter 3/app.js
@@ -1,8 +1,27 @@
-const title = document.querySelector(".hello h1")
+const title = document.querySelector("div.hello:first-child h1")
 // querySelector -> 오로지 첫번째것만 console에 띄워준다.
 // querySelectorAll -> 인자에 적합한 모든 것을 console에 출력해준다.
 
-title.innerText = "Hello";
-console.log(title);
+// title.innerText = "Hello";
 
- 
+function handleTitleClick() {
+  title.style.color = "blue"
+  console.log("title was clicked!");
+}
+
+function handleMouseEnter() {
+  title.style.color = "red"
+  title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+  title.style.color = "green"
+  title.innerText = "Mouse is gone!";
+}
+
+// 웹 사이트에서 클릭 횟수를 인지하는 방법
+title.onclick = handleTitleClick
+title.onmouseenter = handleMouseEnter;
+title.addEventListener("mouseleave", handleMouseLeave);
+
+//  3. 5
