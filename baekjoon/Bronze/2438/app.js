5fc9be8eecb37b317f7d01bb080bc8d584b8537d
구현우 <kjklovekhw@gmail.com>
2024-04-07 21:57:32 +0900

    20240407

--- baekjoon/Bronze/2438/app.js
+++ baekjoon/Bronze/2438/app.js
@@ -1,3 +1,8 @@
 const fs = require('fs');
 const input = fs.readFileSync('input.txt').toString().split('\n');
 // const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+answer = []
+
+for(let i =1; i <= input; i++) answer[i-1] = '*'.repeat(i)
+console.log(answer.join("\n"))
