370da52cd3e1e5ff310ca7dc8eec197cc1b23554
구현우 <kjklovekhw@gmail.com>
2024-04-08 16:52:21 +0900

    20240408

---
+++ baekjoon/silver/2422/sol.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = fs.readFileSync('input.txt').toString().split(' ');
+// const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+
+
+const N = parseInt(input[0])
+const M = parseInt(input[1])
+for(i = 0; i < )
+
