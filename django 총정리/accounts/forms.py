1ace91da097a57c7a41b4931ff5f2f871e38e2c0
구현우 <kjklovekhw@gmail.com>
2024-04-09 15:38:19 +0900

    20240409

--- django 총정리/accounts/forms.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import get_user_model
-
-class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        # 현재 활성화 되어 있는 User 모델로 변경
-        model = get_user_model()
