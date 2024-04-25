e676dfb515e401a642c9cf6ba30a64bfef36ce9f
구현우 <kjklovekhw@gmail.com>
2024-04-25 16:47:21 +0900

    20240425

--- 개인적으로 따로 공부하는_BOJ/스택, 큐, 덱/28278/sol.py
+++ 개인적으로 따로 공부하는_BOJ/스택, 큐, 덱/28278/sol.py
@@ -1,30 +1,33 @@
 import sys
-sys.stdin = open('input.txt')
 
-N = int(input())
+# sys.stdin = open('input.txt')
+
+N = int(sys.stdin.readline())
+
 stack = []
+
 for tc in range(1, N+1):
-    command = input().rstrip()
 
-    if len(command) > 2:
-        stack.append(int(command[2:]))
+    command = sys.stdin.readline().split()
+    if command[0] == '1':
+        stack.append(command[1])
 
-    elif command == '2':
+    elif command[0] == '2':
         if len(stack) == 0:
             print(-1)
         else:
             print(stack.pop())
 
-    elif command == '3':
+    elif command[0] == '3':
         print(len(stack))
 
-    elif command == '4':
-        if stack:
-            print(0)
-        else:
+    elif command[0] == '4':
+        if len(stack) == 0:
             print(1)
+        else:
+            print(0)
 
-    elif command == '5':
+    elif command[0] == '5':
         if len(stack) == 0:
             print(-1)
         else:
