75a12b7dad210edc607270656e16761aa8de83dc
구현우 <kjklovekhw@gmail.com>
2024-05-03 16:45:55 +0900

    README 추가

--- project8/버전2_영화/08-pjt-skelethon/movies/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "movies"
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("filter-genre/", views.filter_genre, name="filter_genre"),
-    path("recommended/", views.recommended, name="recommended"),
-]
