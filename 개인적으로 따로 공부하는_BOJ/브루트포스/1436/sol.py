8f77e1a6b2025908c59fb4f407a735a2d36c881c
구현우 <kjklovekhw@gmail.com>
2024-05-05 23:29:14 +0900

    백준 1346

--- 개인적으로 따로 공부하는_BOJ/브루트포스/1436/sol.py
+++ 개인적으로 따로 공부하는_BOJ/브루트포스/1436/sol.py
@@ -1,6 +1,17 @@
 import sys
 sys.stdin = open('input.txt')
 
-N = int(input())
+n = int(input())
+cnt= 0 
+result = 666
 
-pass
+while True:
+    if '666' in str(result):
+        cnt += 1
+        
+    if cnt == n:
+        break
+    
+    result += 1
+    
+print(result)
