d76e904bf031952874dbfda29f841acb8ff2a6d5
구현우 <kjklovekhw@gmail.com>
2024-05-06 23:15:18 +0900

    백준 11659

--- 개인적으로 따로 공부하는_BOJ/누적 합/11659/sol.py
+++ 개인적으로 따로 공부하는_BOJ/누적 합/11659/sol.py
@@ -1,2 +1,19 @@
+# import sys
+# sys.stdin = open('input.txt')
+
+
 import sys
-sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+m, n = map(int, input().split())
+arr = list(map(int, input().split()))
+p_sum = [0]
+
+temp = 0
+for i in arr:
+    temp += 1
+    p_sum.append(temp)
+    
+for i in range(n):
+    a, b = map(int, input().split())
+    print(p_sum[b]-p_sum[a-1])
