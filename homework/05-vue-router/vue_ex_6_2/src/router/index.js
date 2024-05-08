147eb1f367864fd11e1005bc053c3f476301a3d5
구현우 <kjklovekhw@gmail.com>
2024-05-08 16:41:07 +0900

    Vue 05/08 과제

---
+++ homework/05-vue-router/vue_ex_6_2/src/router/index.js
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import UserView from '@/views/UserView.vue'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView
+    },
+    {
+      path: '/user/:username',
+      name: 'user',
+      component: UserView,
+      beforeEnter: (to, from) => {
+        const username = to.params.username
+        console.log(to)
+        if (username !== 'admin') {
+          window.confirm('admin이 아닙니다')
+          return { name:'home' }
+        } 
+      
+      }
+    }
+  ]
+})
+
+export default router
---
+++ homework/05-vue-router/vue_ex_6_2/src/views/UserView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>User Profile</h1>
+    <h3>username: {{ username }}</h3>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const username = ref(route.params.username)
+</script>
+
+<style scoped>
+
+</style>
