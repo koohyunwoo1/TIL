e5f54c35030765f3d246c1c42eaa879d041de1cf
구현우 <kjklovekhw@gmail.com>
2024-05-09 11:12:26 +0900

    Vue State Mnangement Pinia

---
+++ 06-vue-state-management/vue-project/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <h1>Todo Project</h1>
+    <h2>완료된 Todo : {{ store.doneTodosCount }}</h2>
+    <TodoForm />
+    <TodoList />
+
+
+    <!-- 
+    <p>
+      {{ newNubmer }}
+    </p>
+
+    <p>
+      {{ store.count }}
+    </p>
+
+    <p>
+      {{ store.doubleCount }}
+    </p>
+
+    <button @click="store.increment()">+++</button> -->
+
+
+  </div>
+</template>
+
+<script setup>
+// import { ref, watchEffect } from 'vue'
+// import { useCounterStore } from '@/stores/counter'
+
+import TodoForm from '@/components/TodoForm.vue';
+import TodoList from '@/components/TodoList.vue';
+import { useCounterStore } from '@/stores/counter'
+
+const store = useCounterStore()
+
+// const store = useCounterStore()
+
+// // state
+// console.log(store.count)
+// const newNubmer = store.count + 
+// // const newNubmer = ref(store.count + 1)
+
+// // getters
+// console.log(store.doubleCount)
+
+
+// // watchEffect(() => {
+// //   newNubmer.value = store.count + 1
+// // })
+
+
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ 06-vue-state-management/vue-project/src/components/TodoList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <TodoListItem 
+    v-for="todo in todos"
+    :key="todo.id"
+    :todo="todo"
+    
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import TodoListItem from '@/components/TodoListItem.vue';
+import { useCounterStore } from '@/stores/counter'
+
+const store = useCounterStore()
+const todos = store.todos
+
+
+</script>
+
+<style scoped>
+
+</style>
