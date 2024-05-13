3dcc5d90001e7d77e2bbd6929b4cb18ac87fb287
구현우 <kjklovekhw@gmail.com>
2024-05-13 12:02:02 +0900

    Vue / django

---
+++ 07-01-vue-with-drf/vue-project/src/views/ArticleView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Article Page</h1>
+    <RouterLink :to="{name : 'CreateView'}">
+    [CREATE]
+    </RouterLink>
+    <ArticleList />
+  </div>
+</template>
+
+<script setup>
+import ArticleList from '@/components/ArticleList.vue';
+import  { onMounted } from 'vue'
+import { useCounterStore } from '@/stores/counter';
+import { RouterView, RouterLink } from 'vue-router'
+
+const store = useCounterStore()
+
+onMounted(() => {
+  store.getArticles()
+})
+</script>
+
+<style>
+
+</style>
