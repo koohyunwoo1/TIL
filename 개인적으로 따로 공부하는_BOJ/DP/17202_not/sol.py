f5c62e63a905f17ac610dbab76dbfd629af469f8
구현우 <kjklovekhw@gmail.com>
2024-06-30 18:35:01 +0900

    백준 17202

--- 개인적으로 따로 공부하는_BOJ/DP/17202_not/sol.py
+++ 개인적으로 따로 공부하는_BOJ/DP/17202_not/sol.py
@@ -1,7 +1,23 @@
 import sys
 sys.stdin = open('input.txt')
 
-A = int(input())
-B = int(input())
+A = list(map(int, input()))
+B = list(map(int, input()))
+arr = [0]*16
+for i in range(16):
+    if i % 2 == 0:
+        arr[i] = A[i//2]
+    else:
+        arr[i] = B[i//2]
 
+#print(arr)
+
+while len(arr) != 2:
+    temp = []
+    for i in range(len(arr)-1):
+        num = (arr[i]+arr[i+1]) % 10
+        temp.append(num)
+    arr = temp
+
+print(*arr, sep="")
 
