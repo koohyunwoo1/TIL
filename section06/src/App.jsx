5fd762aab88cf928ca2049c2b7a066f23d4e48cd
구현우 <kjklovekhw@gmail.com>
2024-07-02 16:23:03 +0900

    React Mini Project clear

--- section06/src/App.jsx
+++ section06/src/App.jsx
@@ -1,7 +1,26 @@
 import "./App.css";
+import Viewer from "./components/Viewer";
+import Controller from "./components/Countroller";
+import { useState } from "react";
 
 function App() {
-  return <>카운터 앱</>;
+  const [count, setCount] = useState(0);
+
+  const onClickButton = (value) => {
+    setCount(count + value);
+  };
+
+  return (
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer count={count} />
+      </section>
+      <section>
+        <Controller onClickButton={onClickButton} />
+      </section>
+    </div>
+  );
 }
 
 export default App;
