4fbae3c8b8089c0d4b62131d2501fd564d1ce503
구현우 <kjklovekhw@gmail.com>
2024-07-02 17:48:50 +0900

    section08 UI 구성

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import Editor from "./components/Editor";
+import Header from "./components/Header";
+import List from "./components/List";
 
 function App() {
-  return <>TodoList</>;
+  return (
+    <div className="App">
+      <Header />
+      <Editor />
+      <List />
+    </div>
+  );
 }
 
 export default App;
---
+++ section08/src/components/Editor.jsx
@@ -0,0 +1,12 @@
+import "./Editor.css";
+
+const Edit = () => {
+  return (
+    <div className="Editor">
+      <input placeholder="새로운 Todo..." />
+      <button>추가</button>
+    </div>
+  );
+};
+
+export default Edit;
