be84733d9d641a266c9bb44a0432364697f9012c
구현우 <kjklovekhw@gmail.com>
2024-07-03 17:02:23 +0900

    section12_09

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,35 +1,130 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useReducer, useRef, createContext } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import Edit from "./pages/edit";
+import NotFound from "./pages/NotFound";
 
+const MockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+];
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        String(item.id) === String(action.data.id) ? action.data : item
+      );
+    case "DELETE":
+      return state.filter((item) => String(item.id) !== String(action.id));
+  }
+}
+
+const DiaryStateContext = createContext();
+const DiaryDispatchContext = createContext();
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, dispatch] = useReducer(reducer, MockData);
+  const idRef = useRef(3);
+  // 새로운 일기 추가
+  const onCreate = (createdDate, emotionId, content) => {
+    // 새로운 일기를 추가하는 기능
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+  // 기존 일기 수정
+  const onUpdate = (id, createdDate, emotionId, content) => {
+    // 기존 일기를 수정하는 기능
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+  // 기존 일기 삭제
+  const onDelete = (id) => {
+    // 기존 일기를 삭제하는 기능
+    dispatch({
+      type: "DELETE",
+      id,
+    });
+  };
+  //   const nav = useNavigate();
+
+  //   const onClickButton = () => {
+  //     nav("/new");
+  //   };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button
+        onClick={() => {
+          onCreate(new Date().getTime(), 1, "Hello");
+        }}
+      >
+        일기 추가 테스트
+      </button>
+
+      <button
+        onClick={() => {
+          onUpdate(1, new Date().getTime(), 3, "수정된 일기입니다.");
+        }}
+      >
+        일기 수정 테스트
+      </button>
+      <button
+        onClick={() => {
+          onDelete(1);
+        }}
+      >
+        일기 삭제 테스트
+      </button>
+      {/* // Routes 컴포넌트 바깥에 작성할시 모든 페이지에서 렌더링이된다. */}
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider
+          value={{
+            onCreate,
+            onDelete,
+            onUpdate,
+          }}
+        >
+          <Routes>
+            {/* Routes컴포넌트 안에는 Route컴포넌트만 들어갈수있다. */}
+            <Route path="/" element={<Home />} />
+            <Route path="/New" element={<New />} />
+            <Route path="/Diary/:id" element={<Diary />} />
+            <Route path="/Edit/:id" element={<Edit />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
