767ea8c16fafb3207804658a9709c8d8e3eb88b3
구현우 <kjklovekhw@gmail.com>
2024-07-03 09:12:22 +0900

    section08 Todo Project claer

--- section08/README.md
+++ section08/README.md
@@ -6,3 +6,9 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+### display : flex를 이용해 우리가 원하는 요소들을 가로로 배치할수 있는 방법에 대해서 알게됨
+
+### App.jsx에서 배열형태의 데이터를 만들어서 그 데이터를 리스트의 형태로 렌더링하는 법을 알게됨
+
+### 추가 / 제거 / 수정 / 검색 하는 기능까지 알게됨
--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -45,11 +45,28 @@ function App() {
     // 기존에 있는 todos 배열
   };
 
+  const onUpdate = (targetId) => {
+    // todos State의 값들 중에
+    // targetId와 일치하는 id를 갖는 투두 아이템의 isDone 변경
+
+    // 인수 : todos 배열에서 targetId와 일치하는 id를 갖는 요소의 데이터만 딱 바꾼 새로운 배열
+    setTodos(
+      todos.map((todo) =>
+        todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const onDelete = (targetId) => {
+    // 인수 : todos 배열에서 targetId와 일치하는 id를 갖는 요소만 삭제한 새로운 배열
+    setTodos(todos.filter((todo) => todo.id !== targetId));
+  };
+
   return (
     <div className="App">
       <Header />
       <Editor onCreate={onCreate} />
-      <List todos={todos} />
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
     </div>
   );
 }
