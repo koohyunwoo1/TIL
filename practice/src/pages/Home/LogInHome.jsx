6d24714603beb860f0ecab513103844311292fe1
구현우 <kjklovekhw@gmail.com>
2024-07-18 10:36:43 +0900

    start

---
+++ practice/src/pages/Home/LogInHome.jsx
@@ -0,0 +1,12 @@
+import "../../styles/LogInHome.css";
+import LogOutHeader from "./LogOutHeader";
+
+const LogInHome = () => {
+  return (
+    <div>
+      <LogOutHeader />
+    </div>
+  );
+};
+
+export default LogInHome;
