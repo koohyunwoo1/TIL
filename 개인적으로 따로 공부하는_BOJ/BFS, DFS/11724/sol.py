bf8e1cd51f938565bd23bd997ea565eb949b90dc
구현우 <kjklovekhw@gmail.com>
2024-08-01 23:50:12 +0900

    백준 11724

--- 개인적으로 따로 공부하는_BOJ/BFS, DFS/11724/sol.py
+++ 개인적으로 따로 공부하는_BOJ/BFS, DFS/11724/sol.py
@@ -1,16 +1,26 @@
-from sys import stdin
-read = stdin.readline
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
 
-# 출발지를 주지 않음.
+# dfs 함수
+def dfs(graph, v, visited):
+    visited[v] = True
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
 
-N, M = map(int, read().split())
+n, m = map(int, input().split()) # 정점의 개수, 간선의 개수
+graph = [[] for _ in range(n+1)]
+for i in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
 
-for _ in range(M):
-    left, right = map(int, read().split())
+count = 0 # 연결 노드의 수
+visited = [False] * (n+1)
+for i in range(1, n+1):
+    if not visited[i]:
+        dfs(graph, i, visited)
+        count += 1 # dfs 한 번 끝날 때마다 count+1
 
-    graph = [[0] for _ in range(N + 1)]
-    graph[left].append(right)
-    graph[right].append(left)
-
-visited = [False] * (N+1)
-
+print(count)
