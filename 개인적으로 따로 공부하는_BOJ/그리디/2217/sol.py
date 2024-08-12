9f4d2c23e6d2f54c239494b4ddabfd13d8402768
구현우 <kjklovekhw@gmail.com>
2024-08-12 23:24:12 +0900

    백준 2217

---
+++ 개인적으로 따로 공부하는_BOJ/그리디/2217/sol.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+k = []
+for _ in range(n):
+    k.append(int(input()))
+k.sort()
+
+answers = []
+for x in k:
+    answers.append(x*n)
+    n -= 1
+print(max(answers))
