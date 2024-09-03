5ae54aab9ac35250f059829c192fda81c9784f5f
구현우 <kjklovekhw@gmail.com>
2024-09-03 16:14:55 +0900

    구급 상자 css

--- misc/kakaoLogin/src/components/Box.jsx
+++ misc/kakaoLogin/src/components/Box.jsx
@@ -1,27 +1,42 @@
-// Box.js
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/Box.css";
 
 const Box = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isBubbleVisible, setIsBubbleVisible] = useState(false);
 
-  const toggleLid = () => {
-    setIsOpen((prevState) => !prevState);
-  };
+  const supplements = [
+    "비타민 C",
+    "오메가-3 피쉬 오일",
+    "칼슘",
+    "프로바이오틱",
+    "마그네슘",
+  ];
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsBubbleVisible(true);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <div className="box">
-      <div className={`lid ${isOpen ? "open" : ""}`}></div>
-      <div className="content">
-        <div className="cross">
-          <div className="horizontal"></div>
-          <div className="vertical"></div>
+    <div style={{ marginTop: "500px" }}>
+      <div className="box">
+        <div className="lid open"></div>
+        <div className="content">
+          <div className="cross">
+            <div className="horizontal"></div>
+            <div className="vertical"></div>
+          </div>
+          <div className="label">My Kit</div>
         </div>
-        <div className="label">First Aid</div>
+        <div className={`bubble ${isBubbleVisible ? "show" : ""}`}>
+          {supplements.map((supplement, index) => (
+            <p key={index}>{supplement}</p>
+          ))}
+        </div>
       </div>
-      <button onClick={toggleLid} className="toggle-button">
-        {isOpen ? "Close Lid" : "Open Lid"}
-      </button>
     </div>
   );
 };
