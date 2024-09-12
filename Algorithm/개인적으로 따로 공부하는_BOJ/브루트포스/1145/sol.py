926c7685227bdc89f5c2065392b884a8e599c538
구현우 <kjklovekhw@gmail.com>
2024-09-12 17:21:09 +0900

    백준 1145

---
+++ Algorithm/개인적으로 따로 공부하는_BOJ/브루트포스/1145/sol.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+a = list(map(int, input().split()))
+min = min(a)
+while True:
+    cnt = 0
+    for i in a:
+        if min % i == 0:
+            cnt += 1
+    if cnt > 2:
+        break
+    min  += 1
+print(min)
