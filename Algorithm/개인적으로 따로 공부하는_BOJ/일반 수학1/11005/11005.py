2253b3a8d9193f03242c397e86831342b958380a
구현우 <kjklovekhw@gmail.com>
2024-09-15 22:30:01 +0900

    백준 11005

--- Algorithm/개인적으로 따로 공부하는_BOJ/일반 수학1/11005/11005.py
+++ Algorithm/개인적으로 따로 공부하는_BOJ/일반 수학1/11005/11005.py
@@ -1,4 +1,13 @@
 import sys
-sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
 
 N, B = map(int, input().split())
+s = ''
+arr = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+while N:
+    s += str(arr[N%B])
+    N //= B
+
+print(s[::-1])
