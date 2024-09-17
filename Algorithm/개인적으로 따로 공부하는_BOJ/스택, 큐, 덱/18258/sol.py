57c0532cfb1684e7fe6d4bd77aaf6932643b0590
구현우 <kjklovekhw@gmail.com>
2024-09-17 17:04:06 +0900

    백준 18258

--- Algorithm/개인적으로 따로 공부하는_BOJ/스택, 큐, 덱/18258/sol.py
+++ Algorithm/개인적으로 따로 공부하는_BOJ/스택, 큐, 덱/18258/sol.py
@@ -1,6 +1,29 @@
 import sys
-sys.stdin = open('input.txt')
-
-N = int(input())
-
-
+n = int(input())
+queue = []
+for i in range(n):
+    com = sys.stdin.readline().split()
+    if com[0] == 'push':
+        queue.append(com[1])
+    elif com[0] == 'pop':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue.pop(0))  
+    elif com[0] == 'size':
+        print(len(queue))
+    elif com[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    elif com[0] == 'front':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+    elif com[0] == 'back':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[-1])
