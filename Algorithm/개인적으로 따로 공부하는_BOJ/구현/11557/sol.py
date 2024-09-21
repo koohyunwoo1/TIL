cd930b81ed86f4f181be3f8eafb9c9703ff32e03
구현우 <kjklovekhw@gmail.com>
2024-09-21 21:31:34 +0900

    백준 11557

---
+++ Algorithm/개인적으로 따로 공부하는_BOJ/구현/11557/sol.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    max = 0
+    mName = ""
+    for _ in range(N):
+        name, num = input().split()
+        num = int(num)
+        if(num > max):
+            max = num
+            mName = name
+    print(mName)
