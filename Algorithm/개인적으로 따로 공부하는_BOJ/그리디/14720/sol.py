3b9a804fa193b18d360167a71209f7a683d78f9c
구현우 <kjklovekhw@gmail.com>
2024-09-25 17:45:07 +0900

    백준 14720

---
+++ Algorithm/개인적으로 따로 공부하는_BOJ/그리디/14720/sol.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+milk = list(map(int,input().split()))
+
+count = 0
+
+for i in range(n):
+    if milk[i] == count % 3:
+        count += 1
+
+print(count)
