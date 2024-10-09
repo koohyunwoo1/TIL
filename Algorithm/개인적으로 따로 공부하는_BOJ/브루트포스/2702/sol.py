0420378136c83a8bb9551219336bd05f6b345882
구현우 <kjklovekhw@gmail.com>
2024-10-09 22:03:15 +0900

    백준 2702

---
+++ Algorithm/개인적으로 따로 공부하는_BOJ/브루트포스/2702/sol.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+def lcm(a, b):
+    return int((a * b) / gcd(a, b))
+
+def gcd(a, b):
+    if a == 0:
+        return b
+    else:
+        return gcd(b % a, a)
+
+t = int(input())
+for i in range(t):
+    a, b = map(int, input().split())
+    print(lcm(a, b), gcd(a, b))
