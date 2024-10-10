80ef079e5d91dfd92c671e3d38e55bcad36e5f9a
구현우 <kjklovekhw@gmail.com>
2024-10-10 17:45:37 +0900

    백준 11170

---
+++ Algorithm/개인적으로 따로 공부하는_BOJ/브루트포스/11170/sol.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for i in range(t):
+    cnt = 0
+    n, m = map(int, input().split())
+    for i in range(n, m+1):
+        w = str(i)
+        cnt += w.count('0')
+    print(cnt)
