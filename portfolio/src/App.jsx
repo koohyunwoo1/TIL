e66878cc2d1481494b9c81876b88c92e85d8a5a4
구현우 <kjklovekhw@gmail.com>
2024-10-15 10:19:03 +0900

    PortFolio 캐러셀 라이브러리 추가

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import Home from "../src/pages/Home/Home";
 import Project from "../src/pages/Project/Project";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
--- portfolio/src/components/Project/ProjectItem.jsx
+++ portfolio/src/components/Project/ProjectItem.jsx
@@ -1,58 +1,83 @@
-import React from "react";
-import GflexImage from "../../assets/Gflex.png";
-import PillSooImage from "../../assets/PillSoo.png";
-import RunUsImage from "../../assets/RunUs.png";
+import React, { Component } from "react";
+import Slider from "react-slick";
 import "../../style/Project/ProjectItem.css";
 
-const ProjectItem = () => {
-  const projects = [
-    {
-      title: "Gflex",
-      image: GflexImage,
-      description: "기분/무드별 영화 추천 서비스입니다.",
-      period: "2023.01 - 2023.06",
-      link: "https://github.com/koohyunwoo1/G-flex",
-    },
-    {
-      title: "RunUs",
-      image: RunUsImage,
-      description: "함께 뛰는 러닝 서비스입니다.",
-      period: "2023.07 - 2023.12",
-      link: "https://github.com/koohyunwoo1/RunUs",
-    },
-    {
-      title: "PillSoo",
-      image: PillSooImage,
-      description: "사용자 기반 영양제 추천 서비스입니다.",
-      period: "2023.01 - 2023.06",
-      link: "https://github.com/koohyunwoo1/PillSoo",
-    },
-  ];
+const projects = [
+  {
+    title: "Gflex",
+    images: Array.from(
+      { length: 12 },
+      (_, index) => `/assets/Gflex/image${index}.png`
+    ),
+    description: "기분/무드별 영화 추천 서비스입니다.",
+    period: "2023.01 - 2023.06",
+    link: "https://github.com/koohyunwoo1/G-flex",
+  },
+  {
+    title: "PillSoo",
+    images: Array.from(
+      { length: 12 },
+      (_, index) => `/assets/PillSoo/image${index}.jpg`
+    ),
+    description: "사용자 기반 영양제 추천 서비스입니다.",
+    period: "2023.01 - 2023.06",
+    link: "https://github.com/koohyunwoo1/PillSoo",
+  },
+  {
+    title: "RunUs",
+    images: Array.from(
+      { length: 12 },
+      (_, index) => `/assets/RunUs/image${index}.gif`
+    ),
+    description: "함께 뛰는 러닝 서비스입니다.",
+    period: "2023.07 - 2023.12",
+    link: "https://github.com/koohyunwoo1/RunUs",
+  },
+];
 
-  return (
-    <div className="projectItem">
-      {projects.map((project) => (
-        <div key={project.title} className="projectCard">
-          <h2>{project.title}</h2>
-          <p className="projectPeriod">{project.period}</p>
-          <div className="carousel">
-            <img src={project.image} alt={project.title} />
+class ProjectItem extends Component {
+  render() {
+    const settings = {
+      dots: false,
+      infinite: false,
+      speed: 1,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    };
+
+    return (
+      <div className="projectItem">
+        {projects.map((project) => (
+          <div key={project.title} className="projectCard">
+            <h2>{project.title}</h2>
+            <p className="projectPeriod">{project.period}</p>
+            <div className="carousel">
+              <Slider {...settings}>
+                {project.images.map((image, index) => (
+                  <div key={index} className="carouselImage">
+                    <img src={image} alt={`${project.title} ${index}`} />
+                  </div>
+                ))}
+              </Slider>
+            </div>
+            <div className="projectDescription">
+              <p>{project.description}</p>
+              <a
+                href={project.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="modalLink"
+              >
+                GitHub 링크
+              </a>
+            </div>
           </div>
-          <div className="projectDescription">
-            <p>{project.description}</p>
-            <a
-              href={project.link}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="modalLink"
-            >
-              GitHub 링크
-            </a>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-};
+        ))}
+      </div>
+    );
+  }
+}
 
 export default ProjectItem;
