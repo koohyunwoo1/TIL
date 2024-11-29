1b736277dce67d675639141332f973b9ffbce6a1
구현우 <kjklovekhw@gmail.com>
2024-11-29 10:33:55 +0900

    포트폴리오 제거

--- portfolio/src/components/Home/Header.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "../../style/Components/Header.css";
-const Header = () => {
-  return <h1 className="Header">Front-end Developer</h1>;
-};
-
-export default Header;
--- portfolio/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    include: ["react-icons/fa", "react-icons/si"], // 사용 중인 react-icons 경로 추가
-  },
-});
