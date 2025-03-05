3543c48611d8ea3939cf21c6b0f2841b2a9b5666
구현우 <kjklovekhw@gmail.com>
2025-03-05 22:33:10 +0900

    백준 30999

---
+++ Algorithm/개인적으로 따로 공부하는_BOJ/구현/30999/sol.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+result = 0
+
+for _ in range(N):
+    opinion = input()
+    agreeCount = opinion.count("O")
+    if agreeCount >= (M // 2) + 1:
+        result += 1
+print(result)
